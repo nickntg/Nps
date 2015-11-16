1c2b2b53c29968412505b784f498939442f9e71f
n.bitounis <n.bitounis@pamediakopes.gr>
2015-11-16 23:24:10 +0200

    * Changed controller and added comments that should show up on help pages.

--- Projects/ETravel.Nps.Service/Controllers/NpsController.cs
+++ Projects/ETravel.Nps.Service/Controllers/NpsController.cs
@@ -1,23 +1,60 @@
 ﻿using System.Runtime.InteropServices;
 using System.Web.Http;
+using System.Web.Http.Description;
 
 namespace ETravel.Nps.Service.Controllers
 {
     /// <summary>
-    /// The NPS controller.
+    /// The NPS service can be used to retrieve, create and update NPS ratings.
     /// </summary>
     public class NpsController : ApiController
     {
         /// <summary>
         /// Returns a list of NPS ratings based on ratable ID and/or ratable type.
         /// </summary>
-        /// <param name="ratable_id">Id of the NPS rating.</param>
+        /// <param name="ratable_id">Ratable id of the NPS rating.</param>
         /// <param name="ratable_type">Type of the NPS rating.</param>
-        /// <returns>List of NPS matching the parameters.</returns>
-        /// <summary>test</summary>
-        /// <remarks>remarks</remarks>
+        /// <returns>List of <seealso cref="Models.Nps">NPS</seealso> matching the parameters.</returns>
         [Route("ratings")]
-        public IHttpActionResult GetRatings(string ratable_id, string ratable_type)
+        public IHttpActionResult GetRatings([Optional] [DefaultParameterValue("empty string")] string ratable_id, [Optional] [DefaultParameterValue("empty string")] string ratable_type)
+        {
+            return NotFound();
+        }
+
+        /// <summary>
+        /// Returns a single NPS rating based on its ID.
+        /// </summary>
+        /// <param name="id">Id of the NPS rating.</param>
+        /// <returns>An <seealso cref="Models.Nps">NPS</seealso> matching the id.</returns>
+        [Route("ratings")]
+        public IHttpActionResult GetRating(string id)
+        {
+            return NotFound();
+        }
+
+        /// <summary>
+        /// Creates a new NPS rating.
+        /// </summary>
+        /// <param name="nps">The NPS to create (id is ignored).</param>
+        /// <returns>Created <seealso cref="Models.Nps">NPS</seealso>.</returns>
+        [Route("ratings")]
+        [HttpPost]
+        [ResponseType(typeof(Models.Nps))]
+        public IHttpActionResult CreateRating([FromBody] Models.Nps nps)
+        {
+            return NotFound();
+        }
+
+        /// <summary>
+        /// Updates an existing NPS rating.
+        /// </summary>
+        /// <param name="id">Id of the NPS to update.</param>
+        /// <param name="nps">The NPS to update.</param>
+        /// <returns>Updated <seealso cref="Models.Nps">NPS</seealso>.</returns>
+        [Route("ratings/{id}")]
+        [HttpPut]
+        [ResponseType(typeof(Models.Nps))]
+        public IHttpActionResult UpdateRating(string id, [FromBody] Models.Nps nps)
         {
             return NotFound();
         }
--- Projects/ETravel.Nps.Service/Models/Nps.cs
+++ Projects/ETravel.Nps.Service/Models/Nps.cs
@@ -6,14 +6,50 @@
         /// ID of the NPS. Assigned by the database backend.
         /// </summary>
         public virtual string id { get; set; }
+
+        /// <summary>
+        /// Ratable type of the NPS.
+        /// </summary>
         public virtual string ratable_type { get; set; }
+
+        /// <summary>
+        /// Ratable ID of the NPS>
+        /// </summary>
         public virtual string ratable_id { get; set; }
+
+        /// <summary>
+        /// NPS score (1-9).
+        /// </summary>
         public virtual int score { get; set; }
+
+        /// <summary>
+        /// Customer comment.
+        /// </summary>
         public virtual string comment { get; set; }
+
+        /// <summary>
+        /// NPS creation date (ISO 8601).
+        /// </summary>
         public virtual string created_at { get; set; }
+
+        /// <summary>
+        /// NPS update date (ISO 8601).
+        /// </summary>
         public virtual string updated_at { get; set; }
+
+        /// <summary>
+        /// Customer language.
+        /// </summary>
         public virtual string language { get; set; }
+
+        /// <summary>
+        /// Brand used by customer.
+        /// </summary>
         public virtual string brand { get; set; }
+
+        /// <summary>
+        /// Customer country.
+        /// </summary>
         public virtual string country { get; set; }
     }
 }
