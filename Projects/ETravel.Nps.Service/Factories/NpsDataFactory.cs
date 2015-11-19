d28577eb1352648a6700afea91a2f624f98e5f2a
n.bitounis <n.bitounis@pamediakopes.gr>
2015-11-19 23:38:40 +0200

    * Added web model->data entity factory.

---
+++ Projects/ETravel.Nps.Service/Factories/NpsDataFactory.cs
@@ -0,0 +1,33 @@
+﻿namespace ETravel.Nps.Service.Factories
+{
+    /// <summary>
+    /// Creates data entities from models.
+    /// </summary>
+    public static class NpsDataFactory
+    {
+        /// <summary>
+        /// Converts a web model to a data entity.
+        /// </summary>
+        /// <param name="nps"></param>
+        /// <returns></returns>
+        public static DataAccess.Entities.Nps FromModel(this Models.Nps nps)
+        {
+            if (nps == null)
+            {
+                return null;
+            }
+
+            return new DataAccess.Entities.Nps
+            {
+                Brand = nps.brand,
+                Comment = nps.comment,
+                Country = nps.country,
+                Id = nps.id,
+                Language = nps.language,
+                RatableId = nps.ratable_id,
+                RatableType = nps.ratable_type,
+                Score = nps.score
+            };
+        }
+    }
+}
